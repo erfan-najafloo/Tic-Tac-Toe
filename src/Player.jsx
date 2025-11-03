11b15ba9b5dd83105c3aabfde28ea06f3a2b7e71
Erfan Najafloo <erfan.najafloo@gmail.com>
2025-11-03 13:59:31 +0330

    feat: Implement dynamic button label for edit mode

--- src/Player.jsx
+++ src/Player.jsx
@@ -3,19 +3,20 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
   let playerName = <span className="player-name"> {name}</span>;
   if (isEditing) {
     playerName = <input type="text" />;
   }
+
   return (
     <li>
       <span className="player">
         {playerName}
 
         <span className="player-symbol">{symbol}</span>
-        <button onClick={handleEditClick}>edit</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </span>
     </li>
   );
