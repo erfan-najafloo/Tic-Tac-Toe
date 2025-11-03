80194330248a517a253c6c583faefc899a8f42f9
Erfan Najafloo <erfan.najafloo@gmail.com>
2025-11-03 10:45:18 +0330

    feat: Implement main content area and player info structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
-function App() {}
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">
+                <input type="text" placeholder="Player-1" />
+              </span>
+              <span className="player-symbol">X</span>
+              <button>edit</button>
+            </span>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">
+                <input type="text" placeholder="Player-2" />
+              </span>
+              <span className="player-symbol">O</span>
+              <button>edit</button>
+            </span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+      log
+    </main>
+  );
+}
 
 export default App;
