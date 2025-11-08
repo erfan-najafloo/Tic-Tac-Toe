ec0bdc2c24db0e83dbc4e8d3828cceae119016bc
Erfan Najafloo <erfan.najafloo@gmail.com>
2025-11-08 19:04:40 +0330

    feat: add active player logic and lift state to parent component - Implemented logic to track the active player. - Lifted activePlayer state to the parent component   for shared access between components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { useState } from "react";
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  function handleSelectPlayer() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player 1" symbol="X" />
-          <Player initialName="player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectPlayer}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       log
     </main>
